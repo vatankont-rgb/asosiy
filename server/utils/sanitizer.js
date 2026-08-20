70de7820776b87484dacb3218eeb38b0c58a9ceb
unknown <vatan202444@gmail.com>
2026-08-20 17:25:13 +0500

    Allow style and align attributes in DOMPurify sanitizer

--- server/utils/sanitizer.js
+++ server/utils/sanitizer.js
@@ -8,7 +8,7 @@ function sanitizeHtml(dirtyHtml) {
   if (!dirtyHtml) return "";
   return purify.sanitize(dirtyHtml, {
     ALLOWED_TAGS: ['b', 'i', 'u', 'strong', 'em', 'p', 'br', 'ul', 'ol', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'a', 'img', 'table', 'tr', 'td', 'th', 'thead', 'tbody', 'iframe', 'span', 'div'],
-    ALLOWED_ATTR: ['href', 'src', 'target', 'class', 'id', 'width', 'height', 'frameborder', 'allowfullscreen', 'alt'],
+    ALLOWED_ATTR: ['href', 'src', 'target', 'class', 'id', 'width', 'height', 'frameborder', 'allowfullscreen', 'alt', 'style', 'align'],
     ALLOWED_URI_REGEXP: /^(?:(?:https?|mailto):|[^a-z]|[a-z+.-]+(?:[^a-z+.\-:]|$))/i,
     ADD_TAGS: ['iframe'],
     ALLOW_UNKNOWN_PROTOCOLS: false
